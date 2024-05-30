f12a79b5d295120e0fd4378d209e2dbd89d40c58
Cane <caneg@ciandt.com>
2024-05-30 17:19:53 +0800

    Initial theme & update ingnore.

---
+++ web/themes/custom/learn_theme/js/learn-theme.js
@@ -0,0 +1,17 @@
+/**
+ * @file
+ * learn_theme behaviors.
+ */
+(function (Drupal) {
+
+  'use strict';
+
+  Drupal.behaviors.learnTheme = {
+    attach (context, settings) {
+
+      console.log('It works!');
+
+    }
+  };
+
+} (Drupal));
