b429ab153197b049b6dab3fa6d277dcc2b59e0c3
Cane <caneg@ciandt.com>
2024-06-25 15:33:03 +0800

    0625:Custom content entity.

---
+++ web/modules/custom/forcontu_entities_message/src/MessageAccessControlHandler.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Drupal\forcontu_entities_message;
+
+use Drupal\Core\Access\AccessResult;
+use Drupal\Core\Entity\EntityAccessControlHandler;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Session\AccountInterface;
+
+/**
+ * Defines the access control handler for the message entity type.
+ *
+ * phpcs:disable Drupal.Arrays.Array.LongLineDeclaration
+ *
+ * @see https://www.drupal.org/project/coder/issues/3185082
+ */
+final class MessageAccessControlHandler extends EntityAccessControlHandler {
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkAccess(EntityInterface $entity, $operation, AccountInterface $account): AccessResult {
+    if ($account->hasPermission($this->entityType->getAdminPermission())) {
+      return AccessResult::allowed()->cachePerPermissions();
+    }
+
+    return match($operation) {
+      'view' => AccessResult::allowedIfHasPermission($account, 'view forcontu_entities_message'),
+      'update' => AccessResult::allowedIfHasPermission($account, 'edit forcontu_entities_message'),
+      'delete' => AccessResult::allowedIfHasPermission($account, 'delete forcontu_entities_message'),
+      default => AccessResult::neutral(),
+    };
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function checkCreateAccess(AccountInterface $account, array $context, $entity_bundle = NULL): AccessResult {
+    return AccessResult::allowedIfHasPermissions($account, ['create forcontu_entities_message', 'administer forcontu_entities_message types'], 'OR');
+  }
+
+}
